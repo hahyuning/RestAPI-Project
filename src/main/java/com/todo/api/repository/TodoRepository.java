cace0ea74f86a706d0f353c4b8502f9afe23f70f
hahyuning <hahyuning@gmail.com>
2022-01-16 21:05:38 +0900

    sql 문법오류 수정

--- src/main/java/com/todo/api/repository/TodoRepository.java
+++ src/main/java/com/todo/api/repository/TodoRepository.java
@@ -4,13 +4,11 @@ import com.todo.api.model.TodoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
 public interface TodoRepository extends JpaRepository<TodoEntity, String> {
 
-    @Query("select * from Todo t where t.userId = :userId")
+    @Query("select t from TodoEntity t where t.userId = :userId")
     List<TodoEntity> findByUserId(@Param("userId") String userId);
 }
